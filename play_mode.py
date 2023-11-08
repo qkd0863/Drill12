84adb58198c2299541618796f5965039c3eb5bb5
TIP <seanseol@naver.com>
2023-11-08 17:17:10 +0900

    Drill12 제출(2018180022 방영규)

---
+++ play_mode.py
@@ -0,0 +1,75 @@
+import random
+
+from pico2d import *
+import game_framework
+
+import game_world
+from grass import Grass
+from boy import Boy
+from ball import Ball
+from zombie import Zombie
+
+
+# boy = None
+
+def handle_events():
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            game_framework.quit()
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            game_framework.quit()
+        else:
+            boy.handle_event(event)
+
+
+def init():
+    global grass
+    global boy
+
+    running = True
+
+    grass = Grass()
+    game_world.add_object(grass, 0)
+
+    boy = Boy()
+    game_world.add_object(boy, 1)
+    game_world.add_collision_pair('boy:ball', boy, None)
+    game_world.add_collision_pair('boy:zombie', boy, None)
+
+    # fill here
+    balls = [Ball(random.randint(100, 1500), 60, 0) for _ in range(30)]
+    game_world.add_objects(balls, 1)
+    for ball in balls:
+        game_world.add_collision_pair('boy:ball', None, ball)
+
+    zombies = [Zombie() for _ in range(5)]
+    game_world.add_objects(zombies, 1)
+    for zombie in zombies:
+        game_world.add_collision_pair('boy:zombie', zombie, None)
+        game_world.add_collision_pair('ball:zombie', zombie, None)
+
+
+def finish():
+    game_world.clear()
+    pass
+
+
+def update():
+    game_world.update()
+    game_world.handle_collisions()
+    # fill here
+
+
+def draw():
+    clear_canvas()
+    game_world.render()
+    update_canvas()
+
+
+def pause():
+    pass
+
+
+def resume():
+    pass
